76a341b223a12a08b1768c8881ae3cc635da1bb6
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-06 13:48:35 +0530

    responsive header done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 function Navbar() {
   const navdata = [
@@ -12,20 +12,61 @@ function Navbar() {
     { id: 8, name: "SignUp", link: "#signup", css: "signup-btn" },
   ];
 
+  const [showHeader, setShowHeader] = useState(false);
+
   return (
-    <header className="nav-links-section">
-      
+    <header
+      className={
+        showHeader ? "fit-window-height nav-links-section" : "nav-links-section"
+      }
+    >
       <nav className="nav-links-container">
         <div className="logo-image-container">
-          <a className="nav-link-anchor">
-            <img className="logo-image" src={"./assets/img/logo.svg"} />
+          <a href="#home" className="nav-link-anchor">
+            <img
+              className="logo-image"
+              src={"./assets/img/logo.svg"}
+              alt="logo"
+            />
           </a>
+          <span
+            className={
+              showHeader
+                ? "nav-link-anchor close show-class"
+                : "nav-link-anchor close"
+            }
+          >
+            <img
+              onClick={() => setShowHeader(false)}
+              className="menu-icon"
+              src={"./assets/img/close.svg"}
+              alt="close"
+            />
+          </span>
+          <span
+            className={
+              showHeader
+                ? "nav-link-anchor hamburger"
+                : "nav-link-anchor hamburger show-class"
+            }
+          >
+            <img
+              onClick={() => setShowHeader(true)}
+              className="menu-icon"
+              src={"./assets/img/hamburger.svg"}
+              alt="close"
+            />
+          </span>
         </div>
-        <ul className="nav-links">
-          {navdata.map((item) => {
+        <ul className={showHeader ? "flex-display nav-links" : "nav-links"}>
+          {navdata.map((item, index) => {
             return (
-              <li className="nav-link-li">
-                <a href={item.link} className={`nav-link-anchor ${item.css}`}>
+              <li key={index} className="nav-link-li">
+                <a
+                  onClick={() => setShowHeader(false)}
+                  href={item.link}
+                  className={`nav-link-anchor ${item.css}`}
+                >
                   {item.name}
                 </a>
               </li>
