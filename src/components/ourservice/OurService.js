5fab10c27f9f5f935ef187c110147700239fcd1a
buggplayer <fk331998@gmail.com>
2023-02-09 14:38:23 +0530

    contect

--- src/components/ourservice/OurService.js
+++ src/components/ourservice/OurService.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { servicesContenPara, servicesContentHeading } from "../../utiliti/Data";
+import {
+  emplyeDetails,
+  serviceData,
+  servicesContenPara,
+  servicesContentHeading,
+  whychooseusDesc,
+  whyChoseus,
+  whyChoseusHeading,
+} from "../../utiliti/Data";
 import "./service.css";
 const OurService = () => {
   const CardDatd = [];
@@ -14,20 +22,18 @@ const OurService = () => {
       </article>
 
       <div className="cards-container">
-        {[1, 2, 3, 4].map((item) => {
+        {serviceData.map((item) => {
           return (
             <article className="card">
               <div className="card-image-container">
                 <img
                   className="card-image"
-                  src={"./assets/img/instagram.svg"}
+                  src={item.icon}
+                  // src={"./assets/img/instagram.svg"}
                 />
               </div>
-              <h1 className="card-heading">Instagram Ads</h1>
-              <p className="card-para">
-                Quisque rhoncus lectus ut lectus hendrerit at convallis lorem
-                ornare pellentesque lobortis hendrerit mattis.
-              </p>
+              <h1 className="card-heading">{item.name}</h1>
+              <p className="card-para">{item.desc}</p>
               <a className="card-link">
                 Learn More{" "}
                 <img
@@ -40,39 +46,32 @@ const OurService = () => {
         })}
       </div>
 
-
-
       <div className="services-details-section">
         <article className="services-details-article ">
-          <p className="services-details-subheading">WHY CHOOSE US</p>
+          <p className="services-details-subheading">{whyChoseus}</p>
           <h2 className="services-details-heading">
-            Boots Your Website Buggplayer!
+          {whyChoseusHeading}
           </h2>
           <p className="service-details-description">
-            Morbi at erat et tortor tempor sodales non eu lacus Donec at gravida
-            nunc vitae volutpat libero fusce sit amet mi quis ipsum tempus
-            gravida in quis felis Aliquam fringilla tempus scelerisque Phasellus
-            viverra tincidunt suscipit. Praesent non arcu vitae augue aliquam
-            eget vitae Pellentesque sem mauris porta eget ultrices eros.
+           {whychooseusDesc}
           </p>
           <button className="btn service-details-btn">Discover More</button>
         </article>
         <div className="service-details-cards-container">
-          {[1, 2, 3].map((item) => {
+          {emplyeDetails.map((item) => {
             return (
               <div className="service-detail-card ">
                 <div className="service-detail-card-box">
-                  <p className="service-detail-card-box-total">
-                    <b className="service-detail-card-box-total-value">49 </b>
+                  <p style={{margin:0}} className="service-detail-card-box-total">
+                    <b className="service-detail-card-box-total-value">{item.total} </b>
                     <span className="service-detail-card-box-total-plus">
                       +
                     </span>
                   </p>
-                  <p className="service-detail-card-box-type">Employees</p>
+                  <p className="service-detail-card-box-type">{item.name}</p>
                 </div>
                 <p className="service-detail-card-description">
-                  Aliquam egestas vehicula sapien cibus sit amet rutrum dolor
-                  molestie.
+                 {item.desc}
                 </p>
               </div>
             );
--- src/utiliti/Data.js
+++ src/utiliti/Data.js
@@ -13,9 +13,56 @@ export const whychooseus = "You Manage The Business, We Handle The Application";
 
 /// SERVICE
 export const servicesContentHeading = "Provide Awesome Services";
-export const servicesContenPara=` Vivamus ac nulla ultrices laoreet neque mollis mi morbi elementum
-mauris sit amet arcu fringilla auctor In eleifend maximus nisi sed
-vulputate.Vivamus ac nulla ultrices 
-`
+export const servicesContenPara = ` We're Together. A full-service agency that builds brands, websites and digital products for good tech companies.  
+`;
 
+export const serviceData = [
+  {
+    id: 1,
+    name: "Web Application Development",
+    desc: "Web development services involve everything related to building a web-based solution – whether it is a simple text page or a complex web application. Many people think of “web development” as just using a variety of programming languages to create the web app code and put everything together",
+    icon: "./assets/img/coding.png",
+  },
+  {
+    id: 2,
+    name: "Mobile Application Development",
+    desc: "Get top-notch mobile applications that run on multiple platforms costs while providing your users with an app that fits their OS. Use this alternative to reduce costs and time-to-market and to reach more users without loss of quality, including:",
+    icon: "./assets/img/app-development.png",
+  },
+  {
+    id: 3,
+    name: "Big Data & Data Analytics Solutions",
+    desc: "Big Data Analytics is the use of specific advanced analytics techniques on big data or extremely high volume dataset to generate business insights. It starts with data collection through pre-defined templates and forms, followed by data curation, data processing, through data analysis.",
+    icon: "./assets/img/monitor.png",
+  },
+  {
+    id: 4,
+    name: "Cloud Services",
+    desc: "Cloud computing services are changing how businesses and public institutions use information technology. Today cloud services are available to meet most any IT need. Although there's great variety among cloud computing services, all such services have certain basic features and benefits in common, and all can be categorized into a few basic cloud service types.",
+    icon: "./assets/img/cloud-service.png",
+  },
+  {
+    id: 5,
+    name: "Consulting",
+    desc: "Software consulting services help plan and implement software improvements as well as staff software development projects with skilled resources. As a software consulting company, ScienceSoft offers expertise built on 3,300+ success stories across varied IT needs.",
+    icon: "./assets/img/consulting.png",
+  },
+  {
+    id: 6,
+    name: "Digital Marketing",
+    desc: "Scalable marketing for every stage of your funnel and every milestone of your sales funnel.",
+    icon: "./assets/img/social-media.png",
+  },
+];
 
+// chose us
+export const whyChoseus = "WHY CHOOSE US";
+export const whyChoseusHeading = "  Boots Your Business with Buggplayer!";
+export const whychooseusDesc = `We have highly skilled engineers with excellent technical knowledge and experience in using latest software standards, tools, platforms, frameworks and technologies and we invest continuously in training and education, to be able to respond to any new technology challenges and demands from our clients.
+We have a proven track of great success stories, we work with clients ranging from Fortune 500 to Forbes 50 and we can offer excellent software development opportunities for your business.`;
+
+export const emplyeDetails = [
+  { id: 1, name: "Developers", total: 10, desc: "REACT NODE EXPRESS JACASCRT" },
+  { id: 2, name: "Projects", total: 18, desc: "all domain like fintech , food , ecommerce , pharmacy" },
+  { id: 3, name: "Clinents", total: 30, desc: "all over the glob we have our clients" },
+];
